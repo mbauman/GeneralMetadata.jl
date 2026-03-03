80406c312385aa6ca1477bffe9b0ec003e5bfcb2
Matt Bauman <mbauman@juliahub.com>
2026-03-03 17:59:10 -0500

    simpler tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,17 +8,19 @@ using Dates: Dates
     meta = GeneralMetadata.metadata()
     all_registered_packages = Pkg.Registry.reachable_registries() |> filter(x->x.name == "General") |> only |> x->x.pkgs
     all_registered_names = Set(values(all_registered_packages) .|> x->x.name)
-    @testset "$pkg" for (pkg, pkginfo) in meta
+    in_meta_but_not_registered = Pair{String,String}[]
+    for (pkg, pkginfo) in meta
         @test pkg in all_registered_names
         reg_info = GeneralMetadata.registered_package_versions(pkg)
         for (ver, verinfo) in pkginfo
-            if (Dates.now() - verinfo["registered"]) < Dates.Day(1)
-                # Recently registered versions might not have propagated to the package servers yet
-                continue
+            if !haskey(reg_info, VersionNumber(ver))
+                push!(in_meta_but_not_registered, (pkg=>ver))
             end
-            @test haskey(reg_info, VersionNumber(ver))
         end
     end
+    @test isempty(in_meta_but_not_registered)
+
+    in_registry_but_not_meta = Pair{String,String}[]
     cd(GeneralMetadata.general_repo()) do
         # Roll the registry back to the commit at the time of the last update
         timestamp = GeneralMetadata.last_update(meta) + Dates.Millisecond(500) # --before is exclusive
@@ -26,14 +28,16 @@ using Dates: Dates
         run(`git checkout --quiet --force $last_commit`)
         reg = Pkg.Registry.RegistryInstance(GeneralMetadata.general_repo())
         last_update = GeneralMetadata.last_update(meta)
-        @testset "$pkg" for (pkg, pkginfo) in reg.pkgs
+        for (pkguuid, pkginfo) in reg.pkgs
             pkg_name = pkginfo.name
-            @test haskey(meta, pkg_name)
-            meta_info = meta[pkg_name]
-            reg_info = GeneralMetadata.registered_package_versions(pkg)
+            meta_info = get(meta, pkg_name, Dict{String,Any}())
+            reg_info = GeneralMetadata.registered_package_versions(pkguuid)
             for (ver, verinfo) in reg_info
-                @test haskey(meta_info, string(ver))
+                if !haskey(meta_info, string(ver))
+                    push!(in_registry_but_not_meta, (pkg_name=>string(ver)))
+                end
             end
          end
      end
+     @test isempty(in_registry_but_not_meta)
 end
