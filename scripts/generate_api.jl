3c0558a36206cfc16a03e6ce5038541921088342
Matt Bauman <mbauman@juliahub.com>
2026-03-23 14:47:44 -0400

    add artifact existence and upstream info to JSON api

--- scripts/generate_api.jl
+++ scripts/generate_api.jl
@@ -10,9 +10,37 @@ function main()
     for (pkg, versions) in allversions
         uuid = GeneralMetadata.uuid_from_name(pkg)
         pkgdir = mkpath(joinpath(root, pkg))
-        versioninfo = sort(OrderedDict(
-                ver => sort(OrderedDict(k=>v for (k,v) in info if k in ("registered", "yanked")))
-            for (ver, info) in versions), by=VersionNumber)
+        versioninfo = OrderedDict{String,Any}()
+        for (ver, info) in versions
+            # Registration dates
+            versioninfo[ver]["registered"] = info["registered"]
+            if haskey(info, "yanked")
+                versioninfo[ver]["yanked"] = info["yanked"]
+            end
+            # Artifact information
+            if haskey(info, "artifact_urls")
+                versioninfo[ver]["has_artifacts"] = isempty(info["artifact_urls"])
+                if haskey(info, "artifact_metadata")
+                    components = []
+                    tracked_urls = Set{String}(info["artifact_urls"])
+                    for m in info["artifact_metadata"]
+                        all_tracked = true
+                        for src in get(m, "sources", [])
+                            if haskey(src, "upstream")
+                                push!(components, src["upstream"])
+                            else
+                                all_tracked = false
+                            end
+                        end
+                        all_tracked && setdiff!(tracked_urls, m["artifact_urls"])
+                    end
+                    versioninfo[ver]["artifacts_tracked"] = isempty(tracked_urls)
+                    versioninfo[ver]["artifact_upstreams"] = components
+                end
+            end
+
+        end
+        versioninfo = sort(versioninfo, by=VersionNumber)
         JSON.json(joinpath(pkgdir, "versions.json"), versioninfo)
         JSON.json(joinpath(pkgdir, "info.json"), (; name=pkg, uuid=string(uuid)))
     end
